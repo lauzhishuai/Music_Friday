f696996f601640cada0b16b6378c29cd3374e5ef
lauzhishuai <1830754094@qq.com>
2020-02-02 14:54:06 +0800

    增加歌曲拖拽播放功能

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -31,7 +31,7 @@
           <div class="progress-wrapper">
             <span class="time time-l">{{format(currentTime)}}</span>
             <div class="progress-bar-wrapper">
-              <progress-bar :percent="percent"></progress-bar>
+              <progress-bar :percent="percent" @percentChange="onProgressBarChange"></progress-bar>
             </div>
             <span class="time time-r">{{format(currentSong.duration)}}</span>
           </div>
@@ -215,6 +215,12 @@ export default {
       const minute = (interval / 60) | 0
       const second = this._pad(interval % 60)
       return `${minute}:${second}`
+    },
+    onProgressBarChange(percent) {
+      this.$refs.audio.currentTime = percent * this.currentSong.duration
+      if (!this.playing) {
+        this.togglePlaying()
+      }
     },
     _getPosAndeScale() {
       const targetWidth = 40
